cf7703286da665d717a2d2995460dceac3eb18b2
Mikolaj <mikolaj.korobczak@gmail.com>
2021-01-21 15:53:16 +0100

    More functions in DB interface

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
-const database = require('./lib/databaseUtils.js');
-var express = require('express');
+const database = require("./lib/databaseUtils.js");
+var express = require("express");
 var app = express();
 var Pool = require("pg").Pool;
 var pool = new Pool({
@@ -10,29 +10,77 @@ var pool = new Pool({
   port: 5432,
 });
 
-database.getRoomState(pool, 'tictactoe', 3781014, (err, res) => {
-    if (err) {
-	console.log(err);
+//examples of database's interface uses
+database.createRoom(pool, "tictactoe", 3781014, 3781014, (err, res) => {
+  if (err) {
+    console.log("createRoom");
+    console.log(err);
+  }
+
+  database.changeStateTicTacToe(
+    pool,
+    3781014,
+    "{{1,2,0},{0,0,0},{2,1,1}}",
+    (err, res) => {
+      if (err) {
+        console.log("changeState");
+        console.log(err);
+      }
+      database.addSecondPlayer(
+        pool,
+        "tictactoe",
+        3781014,
+        1234567,
+        (err, res) => {
+          if (err) {
+            console.log("addsecondplayer");
+            console.log(err);
+          }
+
+          database.getRoomState(pool, "tictactoe", 3781014, (err, res) => {
+            if (err) {
+              console.log("getRoomState");
+              console.log(err);
+            }
+            console.log(res);
+	      database.destroyRoom(pool, 'tictactoe', 3781014, (err,res) => {
+		  if (err) {
+		      console.log('destroyroom');
+		      console.log(err);
+		  }});
+          });
+        }
+      );
     }
-    console.log(res);
-});
-app.set('view engine', 'ejs');
-app.set('views', './views');
-app.use('/styles', express.static(__dirname + '/views/styles/'));
-app.use('/images', express.static(__dirname + '/views/images/'));
-app.use(express.urlencoded({
-    extended: true
-}));
-
-app.get('/', function (req, res) {
-    let games = ['warcaby', 'warcaby', 'warcaby', 'warcaby', 'warcaby', 'warcaby', 'warcaby'];
-    res.render('index', { games: games});
+  );
 });
 
-app.get('/warcaby', function (req, res) {
-    res.render('game-page', { game: 'warcaby'});
+app.set("view engine", "ejs");
+app.set("views", "./views");
+app.use("/styles", express.static(__dirname + "/views/styles/"));
+app.use("/images", express.static(__dirname + "/views/images/"));
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+);
+
+app.get("/", function (req, res) {
+  let games = [
+    "warcaby",
+    "warcaby",
+    "warcaby",
+    "warcaby",
+    "warcaby",
+    "warcaby",
+    "warcaby",
+  ];
+  res.render("index", { games: games });
+});
+
+app.get("/warcaby", function (req, res) {
+  res.render("game-page", { game: "warcaby" });
 });
 app.listen(3000, function () {
-    console.log('Example app listening on port 3000!');
+  console.log("Example app listening on port 3000!");
 });
-
--- create_tictactoe_db.py
+++ create_tictactoe_db.py
@@ -6,17 +6,18 @@ in postgres
 '''
 
 from random import randint
+from random import choice
 file = open('tictactoe.dump', 'w')
 print('DROP TABLE IF EXISTS "tictactoe" CASCADE;', file=file)
-print('CREATE TABLE tictactoe (id integer, state integer[3][3]);', file=file)
-print('COPY tictactoe (id, state) FROM stdin;', file=file)
+print('CREATE TABLE tictactoe (id integer NOT NULL PRIMARY KEY, state integer[3][3], playeroneid int, playertwoid int);', file=file)
+print('COPY tictactoe (id, state, playeroneid, playertwoid) FROM stdin;', file=file)
 for _ in range(100):
     print(randint(int(1e6),int(1e7-1)), '{', sep='\t', end='', file=file)
     for i in range(3):
         print('{', randint(0,2), ',', randint(0,2), ',', randint(0,2), '}', sep='', end='', file=file)
         if i != 2:
             print(',', end='', file=file)
-    print('}', file=file)
+    print('}', randint(int(1e6), int(1e7-1)), choice(['\\N', randint(int(1e6), int(1e7-1))]), sep='\t', file=file)
 
 print('\.', file=file)
 
