7936a5b04fa6885ca5be3ab09ad874c86ca99471
Mikolaj <mikolaj.korobczak@gmail.com>
2021-01-25 15:49:36 +0100

    Socket functions to create, join and leave rooms

--- app.js
+++ app.js
@@ -7,15 +7,6 @@ var upload = multer();
 var server = require("http").Server(app);
 var io = require("socket.io")(server);
 
-var Pool = require("pg").Pool;
-var pool = new Pool({
-  user: "mikolaj",
-  host: "localhost",
-  database: "rooms",
-  password: "",
-  port: 5432,
-});
-
 app.set("view engine", "ejs");
 app.set("views", "./views");
 app.use("/styles", express.static(__dirname + "/views/styles/"));
@@ -67,42 +58,84 @@ app.get("/", function (req, res) {
   if (req.session.name == undefined)
     req.session.name = "cahir" + req.sessionID.slice(0, 4);
 
-<<<<<<< HEAD
   res.render("index", {
     games: games,
     nick: req.session.name,
     sesID: req.sessionID,
   });
-=======
-  res.render('index', { games: games, nick: req.session.name, sesID : req.sessionID});
->>>>>>> 356bccb3c52cddd396afd754eee7132c6d87f27f
 });
 
 app.get("/warcaby", function (req, res) {
   if (req.session.name == undefined)
-<<<<<<< HEAD
     req.session.name = "cahir" + req.sessionID.slice(0, 4);
+
+  //let rooms = io.nsps['/'].adapter.rooms //jak będą pokoje i socket, pracuję na wersji bez socket io
+  // let rooms = io.sockets.adapter.rooms;
+
   res.render("game-page", {
     game: "warcaby",
     nick: req.session.name,
     sesID: req.sessionID,
+    rooms: rooms,
   });
-=======
-    req.session.name = 'cahir' + req.sessionID.slice(0, 4)
-
-  //let rooms = io.nsps['/'].adapter.rooms //jak będą pokoje i socket, pracuję na wersji bez socket io
-  let rooms = {"room1": {sockets: {}, length: 1}, "room2": {sockets: {}, length: 1}, "room3": {sockets: {}, length: 2}};
-
-  res.render('game-page', { game: 'warcaby', nick: req.session.name, sesID : req.sessionID, rooms: rooms});
->>>>>>> 356bccb3c52cddd396afd754eee7132c6d87f27f
 });
+
+var rooms = new Map();
 
 io.on("connection", function (socket) {
   console.log("A user connected!"); // We'll replace this with our own events
 
   socket.on("createRoom", (data) => {
-    socket.join("room-0");
-    socket.emit("newRoom", { name: data.name, room: "room-0" });
+    console.log(data.name + " room created");
+    socket.join(data.sesID);
+    socket.emit("newRoom", { room: data.sesID, name: data.name });
+    rooms = rooms.set(data.sesID, {
+      room: data.sesID,
+      name: data.name,
+      player1: data.sesID,
+      player2: NaN,
+      size: 1,
+    });
+  });
+
+  socket.on("joinRoom", (data) => {
+    console.log(data.roomID + " joining room");
+    console.log(data.player + " is joining");
+    // var room = io.sockets.adapter.rooms.get(data.name);
+    // console.log(io.sockets.adapter.rooms);
+    // console.log(room);
+    var room = rooms.get(data.roomID);
+    if (room && room.size == 1) {
+      console.log("done");
+      socket.join(data.roomID);
+      socket.broadcast.to(data.roomID).emit("player1", {});
+      socket.emit("player2", { room: data.name });
+      room.player2 = data.player;
+      room.size = 2;
+      rooms = rooms.set(data.roomID, room);
+    } else {
+      console.log("this room doesn't exist");
+    }
+  });
+
+  socket.on("disconnecting", (reason) => {
+    console.log(socket.rooms);
+    for (let roomID of socket.rooms) {
+      if (roomID != socket.id) {
+        socket.to(roomID).emit("user has left", roomID);
+        rooms.delete(roomID);
+      }
+    }
+  });
+
+  socket.on("leaveRoom", (data) => {
+    if (!data.forced) {
+      socket.to(data.roomID).emit("user has left", data.roomID);
+      socket.leave(data.roomID);
+      rooms.delete(data.roomID);
+    } else {
+      socket.leave(data.roomID);
+    }
   });
 });
 
--- views/scripts/main.js
+++ views/scripts/main.js
@@ -1,10 +1,32 @@
 (function () {
-    var socket = io();
+  var socket = io();
 
-    $('new').on('click', function() {
-	var name='name';
-	var name = JSON.parse($('#nick').text());
-	$('#variableJSON').remove();
-	socket.emit('createRoom', {name:name});
+  console.log("started");
+
+  $(document).ready(function () {
+    var sesID = JSON.parse($("#sesid").text());
+    var name = JSON.parse($("#nick").text());
+    $("#new").on("click", function () {
+      console.log("new room button");
+      $("#variableJSON").remove();
+      console.log(name);
+      socket.emit("createRoom", { name: name, sesID: sesID });
     });
+    $("button[data-roomID]").on("click", (event) => {
+      console.log("joining room");
+      socket.emit("joinRoom", {
+        roomID: $(event.target).data("roomid"),
+        player: sesID,
+        name: name,
+      });
+    });
+
+    socket.on("user has left", (roomID) => {
+      socket.emit("leaveRoom", {
+        roomID: roomID,
+        player: sesID,
+        forced: true,
+      });
+    });
+  });
 })();
