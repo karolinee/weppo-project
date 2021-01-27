0c51d041df6fe8a46d74b8eb08dc7c8aaaa1cc6e
Mikolaj <mikolaj.korobczak@gmail.com>
2021-01-27 15:11:57 +0100

    working version of connect4

--- views/scripts/main_4connect.js
+++ views/scripts/main_4connect.js
@@ -1,21 +1,31 @@
 (function () {
-  var socket = io('/4connect');
+  var socket = io("/4connect");
 
   console.log("started");
 
   $(document).ready(function () {
+    var translateColumn = new Map();
+    translateColumn.set(1, "a");
+    translateColumn.set(2, "b");
+    translateColumn.set(3, "c");
+    translateColumn.set(4, "d");
+    translateColumn.set(5, "e");
+    translateColumn.set(6, "f");
+    translateColumn.set(7, "g");
     var sesID = JSON.parse($("#sesid").text());
     var name = JSON.parse($("#nick").text());
+    var myTurn = false;
+    var myRoom = sesID;
+    var player;
     $("#new").on("click", function () {
       console.log("new room button");
       $("#variableJSON").remove();
       console.log(name);
       socket.emit("createRoom", { name: name, sesID: sesID });
-	$("#game-board").css("display", "block");
+      $(".list-group").css("display", "none");
+      $("#game-board").css("display", "block");
     });
-      $("#columnOne").on("click", data => {
-	  console.log('dupa');
-      });
+
     $("button[data-roomID]").on("click", (event) => {
       console.log("joining room");
       socket.emit("joinRoom", {
@@ -23,7 +33,9 @@
         player: sesID,
         name: name,
       });
+      myRoom = $(event.target).data("roomid");
       $(".list-group").css("display", "none");
+      $("#game-board").css("display", "block");
     });
 
     socket.on("user has left", (roomID) => {
@@ -32,6 +44,97 @@
         player: sesID,
         forced: true,
       });
+    });
+
+    socket.on("gameStarted", (data) => {
+      console.log("game started" + data.you);
+      player = data.you;
+      myTurn = data.turn == player ? true : false;
+    });
+
+    socket.on("illegalMove", (_) => {
+      console.log("illegalMove");
+      myTurn = true;
+    });
+
+    socket.on("moveMade", (data) => {
+      console.log("now moves " + data.turn);
+      let id = translateColumn.get(data.column) + (data.row + 1);
+      let color = data.turn == 1 ? "red" : "black";
+      $(`#${id}`).attr("class", color);
+      myTurn = data.turn == player;
+    });
+
+    socket.on("gameEnded", (data) => {
+      let id = translateColumn.get(data.column) + (data.row + 1);
+      let color = data.turn == 1 ? "red" : "black";
+      $(`#${id}`).attr("class", color);
+      myTurn = false;
+      //tutaj jakoś trzeba obwieścić kto wygrał
+    });
+
+    $("#columnOne").on("click", (data) => {
+      if (myTurn) {
+        myTurn = false;
+        socket.emit("madeTurn", {
+          column: 1,
+          roomID: myRoom,
+        });
+      }
+    });
+    $("#columnTwo").on("click", (data) => {
+      if (myTurn) {
+        myTurn = false;
+        socket.emit("madeTurn", {
+          column: 2,
+          roomID: myRoom,
+        });
+      }
+    });
+    $("#columnThree").on("click", (data) => {
+      if (myTurn) {
+        myTurn = false;
+        socket.emit("madeTurn", {
+          column: 3,
+          roomID: myRoom,
+        });
+      }
+    });
+    $("#columnFour").on("click", (data) => {
+      if (myTurn) {
+        myTurn = false;
+        socket.emit("madeTurn", {
+          column: 4,
+          roomID: myRoom,
+        });
+      }
+    });
+    $("#columnFive").on("click", (data) => {
+      if (myTurn) {
+        myTurn = false;
+        socket.emit("madeTurn", {
+          column: 5,
+          roomID: myRoom,
+        });
+      }
+    });
+    $("#columnSix").on("click", (data) => {
+      if (myTurn) {
+        myTurn = false;
+        socket.emit("madeTurn", {
+          column: 6,
+          roomID: myRoom,
+        });
+      }
+    });
+    $("#columnSeven").on("click", (data) => {
+      if (myTurn) {
+        myTurn = false;
+        socket.emit("madeTurn", {
+          column: 7,
+          roomID: myRoom,
+        });
+      }
     });
   });
 })();
